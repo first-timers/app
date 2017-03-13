342f4a4c925d1418a30246f827c55a352a861079
agonzalez0515 <agonzalez0515@gmail.com>
2017-03-12 22:26:44 -0700

    create server and index page

---
+++ server.js
@@ -0,0 +1,66 @@
+var Hapi = require('hapi');
+// var Path =
+var Inert = require('inert');
+var Vision = require('vision');
+var Handlebars = require('handlebars')
+
+
+var server = new Hapi.Server();
+
+server.connection({
+  port: 8000,
+  host: 'localhost'
+});
+
+server.register(Inert, function(err) {
+  if (err) {
+    throw err;
+  }
+
+  server.route({
+      method: "GET",
+      path: '/public/{path*}',
+      handler: {
+          directory: {
+              path: "./public",
+              listing: false,
+              index: false
+          }
+      }
+  });
+});
+
+server.register(Vision, function(err){
+  if (err) {
+    throw err;
+  }
+
+  //configure template support
+  server.views({
+    engines: {
+      html: Handlebars
+    },
+    path: './public',
+    //need to figure out how to render partials and use default layout
+    // layoutPath: Path.join(__dirname, 'views/layouts')
+  })
+});
+
+server.route({
+  method: 'GET',
+  path: '/',
+  handler: function (request, reply){
+    var data = {
+      title: 'This is the index page!',
+      message: 'Hello!!!'
+    };
+    return reply.view('index', data);
+  }
+});
+
+server. start((err) => {
+  if (err) {
+    throw err;
+  }
+  console.log(`Server running at: ${server.info.uri}`);
+})
