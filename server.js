d2482ec60564266bf523064aab8ee1721ba76ccd
Gregor <gregor@martynus.net>
2017-09-07 22:45:41 -0700

    feat: log debug information

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
+const debug = require('debug')('probot:first-timers-bot')
+
 const handleCreateEvent = require('./lib/handle-create-event')
 
 module.exports = function (robot) {
+  debug(`ready to receive "create" webhooks`)
   robot.on('create', handleCreateEvent)
 }
