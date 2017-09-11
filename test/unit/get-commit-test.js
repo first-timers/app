c7a508321b186f4b49d12bcf4b7262e5a1ab08d0
agonzalez0515 <agonzalez0515@gmail.com>
2017-09-10 23:12:17 -0700

    test: add getCommit unit test

---
+++ test/unit/get-commit-test.js
@@ -0,0 +1,51 @@
+const simple = require('simple-mock')
+const {test} = require('tap')
+
+const getCommit = require('../../lib/get-commit')
+
+const api = {
+  repos: {
+    getCommit: () => {}
+  }
+}
+
+test('get commit request succeeds', t => {
+  const state = {
+    api,
+    debug: () => {},
+    owner: 'owner',
+    repo: 'repo',
+    sha: 'sha'
+  }
+
+  simple.mock(api.repos,
+  'getCommit').resolveWith({
+    data: {
+      files: [{
+        filename: 'filename',
+        patch: 'patch',
+        blob_url: 'blob_url'
+      }],
+      commit: {
+        message: 'message'
+      }
+    }
+  })
+
+  getCommit(state)
+
+  .then(() => {
+    const getCommitArgs =
+    api.repos.getCommit.lastCall.arg
+    t.is(getCommitArgs.owner, 'owner')
+    t.is(getCommitArgs.repo, 'repo')
+    t.is(getCommitArgs.sha, 'sha')
+    t.is(state.commit.message, 'message')
+    t.is(state.commit.filename, 'filename')
+    t.is(state.commit.patch, 'patch')
+    t.is(state.commit.blobUrl, 'blob_url')
+
+    simple.restore()
+    t.end()
+  })
+})
