12f4234e1afbd4f1b2eec71fc78a4489f0db0d9e
Techforchange <arleneperezsb@gmail.com>
2017-09-15 10:52:04 -0700

    Finalized tests for createIssues

--- test/unit/create-issue-test.js
+++ test/unit/create-issue-test.js
@@ -35,14 +35,18 @@ test('create issue request succeeds', t => {
 
   createIssue(state)
 
-  .then(() => {
-    // const content = state.template
+  .then((response) => {
     const createIssueArgs = api.issues.create.lastCall.arg
-    t.is(createIssueArgs.owner, 'owner')
-    t.is(createIssueArgs.repo, 'repo')
+    // 1) Make sure the issue was created
+    t.is(response.data.html_url, 'html_url')
+    // 2) Make sure it includes the patch, filename,etc
     t.is(createIssueArgs.body, 'test value1: patch value2: filename value3: blobUrl value4: repo')
+    // 3) Make sure the issue was created on the right repo?
+    t.is(createIssueArgs.repo, 'repo')
+    // 4) Issue has the correct label?
     t.is(createIssueArgs.labels, 'labels')
-    // Need to test for html_url and need to make sure it's the correct html_url
+    // 5) That its owner is correct
+    t.is(createIssueArgs.owner, 'owner')
 
     simple.restore()
     t.end()
