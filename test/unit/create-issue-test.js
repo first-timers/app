3cf36446a47503d2f55311b9960506f0d38166aa
techforchange <arleneperezsb@gmail.com>
2017-09-18 16:39:40 -0700

    Creates issue failure test and deletes comments from successful issue test

--- test/unit/create-issue-test.js
+++ test/unit/create-issue-test.js
@@ -37,16 +37,49 @@ test('create issue request succeeds', t => {
 
   .then((response) => {
     const createIssueArgs = api.issues.create.lastCall.arg
-    // 1) Make sure the issue was created
     t.is(response.data.html_url, 'html_url')
-    // 2) Make sure it includes the patch, filename,etc
     t.is(createIssueArgs.body, 'test value1: patch value2: filename value3: blobUrl value4: repo')
-    // 3) Make sure the issue was created on the right repo?
     t.is(createIssueArgs.repo, 'repo')
-    // 4) Issue has the correct label?
     t.is(createIssueArgs.labels, 'labels')
-    // 5) That its owner is correct
     t.is(createIssueArgs.owner, 'owner')
+
+    simple.restore()
+    t.end()
+  })
+})
+
+test('create issue request fails', t => {
+  const state = {
+    api,
+    debug: () => {},
+    owner: 'owner',
+    repo: 'repo',
+    branch: 'branch',
+    sha: 'sha',
+    labels: 'labels',
+    commit: {
+      message: 'title',
+      patch: 'patch',
+      filename: 'filename',
+      blobUrl: 'blobUrl'
+    },
+    template: 'test value1: $DIFF value2: $FILENAME value3: $BLOB_URL value4: $REPO'
+  }
+
+  simple.mock(api.issues, 'create').rejectWith({
+    message: 'Not Found',
+    documentation_url: 'https://developer.github.com/v3',
+    code: 404
+  })
+
+  createIssue(state)
+
+  .then(() => {
+    t.error('should not resolve')
+  })
+
+  .catch((error) => {
+    t.is(error.code, 404)
 
     simple.restore()
     t.end()
