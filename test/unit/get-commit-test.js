1f530460b776f7ce6fdb831d146a7504aba76dca
Jeffrey Warren <jeff@unterbahn.com>
2018-04-30 20:50:23 -0400

    fix: correct path to changed file (#192)

--- test/unit/get-commit-test.js
+++ test/unit/get-commit-test.js
@@ -25,7 +25,7 @@ test('get commit request succeeds', t => {
       files: [{
         filename: 'filename',
         patch: 'patch',
-        blob_url: 'https://github.com/Techforchange/first-timers-test/blob/f00ecb1bbffe515500558568ae0b176d2a1defe8/README.md'
+        blob_url: 'https://github.com/Techforchange/first-timers-test/blob/f00ecb1bbffe515500558568ae0b176d2a1defe8/docs/README.md'
       }],
       commit: {
         message: 'message'
@@ -47,7 +47,7 @@ test('get commit request succeeds', t => {
       t.is(state.commit.message, 'message')
       t.is(state.commit.filename, 'filename')
       t.is(state.commit.patch, 'patch')
-      t.is(state.commit.branchUrl, 'https://github.com/Techforchange/first-timers-test/blob/defaultBranch/README.md')
+      t.is(state.commit.branchUrl, 'https://github.com/Techforchange/first-timers-test/blob/defaultBranch/docs/README.md')
       t.is(state.commit.authorLogin, 'username')
 
       simple.restore()
