713bd29beac23a140b30186433d8862bc3898a48
Gregor <gregor@martynus.net>
2019-03-13 12:23:39 -0700

    fix: `.repos.getContent` -> `.repos.getContents`
    closes #212

--- test/unit/get-template-content-test.js
+++ test/unit/get-template-content-test.js
@@ -5,7 +5,7 @@ const getTemplateContent = require('../../lib/get-template-content')
 
 const api = {
   repos: {
-    getContent: () => {}
+    getContents: () => {}
   }
 }
 
@@ -18,7 +18,7 @@ test('gets template content if URL exists', t => {
     customTemplateUrl: 'custom_url'
   }
 
-  simple.mock(api.repos, 'getContent').resolveWith({
+  simple.mock(api.repos, 'getContents').resolveWith({
     data: {
       content: 'Y29udGVudA=='
     }
@@ -27,7 +27,7 @@ test('gets template content if URL exists', t => {
   getTemplateContent(state)
 
     .then(() => {
-      const getTemplateContentArgs = api.repos.getContent.lastCall.arg
+      const getTemplateContentArgs = api.repos.getContents.lastCall.arg
       t.is(getTemplateContentArgs.owner, 'owner')
       t.is(getTemplateContentArgs.repo, 'issueRepo')
       t.is(getTemplateContentArgs.path, 'custom_url')
